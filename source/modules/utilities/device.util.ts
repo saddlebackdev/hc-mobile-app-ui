316eb26755a6237dc3326d97346c8e0eb4fc550e
Ajay Gupta <ajay.gupta@bushel.co.in>
2021-10-29 16:23:23 +0530

    fixing exports for device util methods

--- source/modules/utilities/device.util.ts
+++ source/modules/utilities/device.util.ts
@@ -1,19 +1,24 @@
 // Modules
 import {Platform} from 'react-native';
 
-// OS
-const isAndroid: Function = (): boolean => {
+/**
+ * @method isAndroid
+ * Returns true if the current OS is Android
+ * */
+export const isAndroid: Function = (): boolean => {
   return Platform.OS === 'android';
 };
-const isIos: Function = (): boolean => {
+
+/**
+ * @method isIos
+ * Returns true if the current OS is Ios
+ * */
+export const isIos: Function = (): boolean => {
   return Platform.OS === 'ios';
 };
 
 // Exports
 export default {
-  /** Returns true if the current OS is Android */
   isAndroid,
-
-  /** Returns true if the current OS is iOS */
   isIos,
 };
