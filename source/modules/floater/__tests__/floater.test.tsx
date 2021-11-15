6d090e45c1a4a61fc4f45fbf5c0dd75fd5ef5d5c
Ajay Gupta <ajay.gupta@bushel.co.in>
2021-11-15 14:10:46 +0530

    fix react imports and moved styled components to peer deps

--- source/modules/floater/__tests__/floater.test.tsx
+++ source/modules/floater/__tests__/floater.test.tsx
@@ -1,6 +1,6 @@
 // npx jest ./source/modules/floater/__tests__/floater.test.ts
 
-import React from 'react';
+import * as React from 'react';;
 import {render} from '@testing-library/react-native';
 
 import Floater from '../floater';
