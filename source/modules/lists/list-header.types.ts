fde4d6dea5b087726b3adcaf969717d9c712cb57
Ajay Gupta <ajay.gupta@bushel.co.in>
2022-01-19 17:29:34 +0530

    add list header component

---
+++ source/modules/lists/list-header.types.ts
@@ -0,0 +1,11 @@
+// Props
+export interface IProps {
+  /** Title of the list. */
+  title?: string;
+
+  /** Label for the link shown on right. */
+  linkLabel?: string;
+
+  /** Function to be called when the link on right is pressed. */
+  onLinkPress?: any;
+}
