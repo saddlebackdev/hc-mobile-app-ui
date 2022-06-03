bd0e691895b0d9f5f826e9dd700654b7feebf8be
Yogesh Patel <yogesh.patel@technossus.com>
2022-06-03 19:57:11 +0530

    Fixed issue in core milestone component

--- src/modules/core-milestone/core-milestone.tsx
+++ src/modules/core-milestone/core-milestone.tsx
@@ -5,18 +5,13 @@ import {IProps} from './core-milestone.types';
 import Icon from '../icon/icon-external';
 import {minorScale} from '../scales';
 import Styled from 'styled-components/native';
-/* @ts-ignore */
-import ShapeHeart from '../../images/shape-heart.svg';
-/* @ts-ignore */
-import Baptism from '../../images/baptism.svg';
-/* @ts-ignore */
-import People from '../../images/people.svg';
-/* @ts-ignore */
-import ServingOpportunity from '../../images/serving-opportunity.svg';
-/* @ts-ignore */
-import ShapeExperiences from '../../images/shape-experiences.svg';
+import IconShapeHeart from '../../images/shape-heart.svg';
+import IconBaptism from '../../images/baptism.svg';
+import IconPeople from '../../images/people.svg';
+import IconServingOpportunity from '../../images/serving-opportunity.svg';
+import IconShapeExperiences from '../../images/shape-experiences.svg';
 
-const StyledViewIconWrapper = Styled.View`
+const StyledViewIconsWrapper = Styled.View`
 flex-direction: row;
 align-items: center;
 margin-vertical: ${minorScale(1)};
@@ -29,16 +24,13 @@ const StyledMilestoneIconWrapper = Styled(Icon)``;
 const StyledClassMilestoneWrapper = Styled.View<IProps>`
 flexDirection: row;
 alignItems: center;
-marginRight: ${props =>
-  props.isWhite === true ? `${minorScale(1)}px` : `${minorScale(1)}px`}};
-marginLeft: ${props =>
-  props.isWhite === true ? `${minorScale(1)}px` : `${minorScale(1)}px`}};;
+margin-horizontal: ${minorScale(1)};
 `;
 
 //left has attended class 301
 const StyledLeftHasAttendedClass301Wrapper = Styled.View<IProps>`
-height: ${Number(minorScale(1)) + 1}px;
-width: ${Number(minorScale(1)) + 1}px;
+height: 6px;
+width: 6px;
 margin-right: -1px;
 background-color:${props =>
   props.hasAttendedClass301 && props.hasSignedMinistryCovenant
@@ -63,114 +55,91 @@ transform: rotate(135deg);
 
 //Top hasAttendedClass201
 const StyledClasshasAttendedClass201Wrapper = Styled.View<IProps>`
-            height: ${Number(minorScale(1)) + 1}px;
-              width: ${Number(minorScale(1)) + 1}px;
-              backgroundColor:${props =>
-                props.hasAttendedClass201 && props.hasSignedMaturityCovenant
-                  ? props.isWhite === true
-                    ? '#fff'
-                    : props.colorMilestone
-                  : props.hasAttendedClass201
-                  ? 'transparent'
-                  : '#dde1e4'};
-              borderRadius: 1px;
-              borderWidth: 0.5px;
-              borderColor:${props =>
-                props.hasAttendedClass201 && props.hasSignedMaturityCovenant
-                  ? 'transparent'
-                  : props.hasAttendedClass201
-                  ? props.isWhite === true
-                    ? '#fff'
-                    : props.colorMilestone
-                  : 'transparent'};
-              transform:rotate(135deg);
+height: 6px;
+width: 6px;
+backgroundColor:${props =>
+  props.hasAttendedClass201 && props.hasSignedMaturityCovenant
+    ? props.isWhite === true
+      ? '#fff'
+      : props.colorMilestone
+    : props.hasAttendedClass201
+    ? 'transparent'
+    : '#dde1e4'};
+borderRadius: 1px;
+borderWidth: 0.5px;
+borderColor:${props =>
+  props.hasAttendedClass201 && props.hasSignedMaturityCovenant
+    ? 'transparent'
+    : props.hasAttendedClass201
+    ? props.isWhite === true
+      ? '#fff'
+      : props.colorMilestone
+    : 'transparent'};
+transform:rotate(135deg);
 `;
 
 // Bottom hasAttendedClass401
 const StyledClassHasAttendedClass401Wrapper = Styled.View<IProps>`
-height: ${Number(minorScale(1)) + 1}px;
-              width: ${Number(minorScale(1)) + 1}px;
-              marginTop: 4;
-              backgroundColor:${props =>
-                props.hasAttendedClass401 && props.hasSignedMissionCovenant
-                  ? props.isWhite === true
-                    ? '#fff'
-                    : props.colorMilestone
-                  : props.hasAttendedClass401
-                  ? 'transparent'
-                  : '#dde1e4'};
-              borderRadius: 1px;
-              borderWidth: 0.5px;
-              borderColor:${props =>
-                props.hasAttendedClass401 && props.hasSignedMissionCovenant
-                  ? 'transparent'
-                  : props.hasAttendedClass401
-                  ? props.isWhite === true
-                    ? '#fff'
-                    : props.colorMilestone
-                  : 'transparent'};
-              transform:rotate(135deg);
+height: 6px;
+width: 6px;
+marginTop: 4;
+backgroundColor:${props =>
+  props.hasAttendedClass401 && props.hasSignedMissionCovenant
+    ? props.isWhite === true
+      ? '#fff'
+      : props.colorMilestone
+    : props.hasAttendedClass401
+    ? 'transparent'
+    : '#dde1e4'};
+borderRadius: 1px;
+borderWidth: 0.5px;
+borderColor:${props =>
+  props.hasAttendedClass401 && props.hasSignedMissionCovenant
+    ? 'transparent'
+    : props.hasAttendedClass401
+    ? props.isWhite === true
+      ? '#fff'
+      : props.colorMilestone
+    : 'transparent'};
+transform:rotate(135deg);
 `;
 
 // Rigth hasAttendedClass101
 const StyledClassHasAttendedClass101Wrapper = Styled.View<IProps>`
-height: ${Number(minorScale(1)) + 1}px;
-            width: ${Number(minorScale(1)) + 1}px;
-            marginLeft: -1px;
-            backgroundColor:${props =>
-              props.hasAttendedClass101 && props.hasSignedMembershipAgreement
-                ? props.isWhite === true
-                  ? '#fff'
-                  : props.colorMilestone
-                : props.hasAttendedClass101
-                ? 'white'
-                : '#dde1e4'};
-            borderRadius: 1px;
-            borderWidth: 0.5px;
-            borderColor:${props =>
-              props.hasAttendedClass101 && props.hasSignedMembershipAgreement
-                ? 'transparent'
-                : props.hasAttendedClass101
-                ? props.isWhite === true
-                  ? '#fff'
-                  : props.colorMilestone
-                : 'transparent'};
-            transform: rotate(135deg);
+height: 6px;
+width: 6px;
+marginLeft: -1px;
+backgroundColor:${props =>
+  props.hasAttendedClass101 && props.hasSignedMembershipAgreement
+    ? props.isWhite === true
+      ? '#fff'
+      : props.colorMilestone
+    : props.hasAttendedClass101
+    ? 'white'
+    : '#dde1e4'};
+borderRadius: 1px;
+borderWidth: 0.5px;
+borderColor:${props =>
+  props.hasAttendedClass101 && props.hasSignedMembershipAgreement
+    ? 'transparent'
+    : props.hasAttendedClass101
+    ? props.isWhite === true
+      ? '#fff'
+      : props.colorMilestone
+    : 'transparent'};
+transform: rotate(135deg);
 `;
 
 const StyledAcceptChristWrapper = Styled.View`
 width: ${minorScale(3)};
 height: ${minorScale(3)};
-margin-horizontal: ${minorScale(1)};
-margin-left:0px;
+margin-right: ${minorScale(1)};
 `;
 
-const StyledBaptisedWrapper = Styled.View`
+const StyledIconViewWrapper = Styled.View`
 width: ${minorScale(3)};
 height: ${minorScale(3)};
 margin-horizontal: ${minorScale(1)};
-margin-left:${minorScale(1)}px;
-`;
-
-const StyledSmallGroupWrapper = Styled.View`
-width: ${minorScale(3)};
-height: ${minorScale(3)};
-margin-horizontal: ${minorScale(1)};
-margin-left:${minorScale(1)}px;
-`;
-
-const StyledInMinistryWrapper = Styled.View`
-width: ${minorScale(3)};
-height: ${minorScale(3)};
-margin-horizontal: ${minorScale(1)};
-margin-left:${minorScale(1)}px;
-`;
-
-const StyledActiveMissionWrapper = Styled.View`
-width: ${minorScale(3)};
-height: ${minorScale(3)};
-margin-horizontal: ${minorScale(1)};
-margin-left:${minorScale(1)}px;
 `;
 
 export const CoreMilestone: React.FC<IProps> = ({
@@ -230,15 +199,15 @@ export const CoreMilestone: React.FC<IProps> = ({
   };
 
   const getColorbaseOnUserType = () => {
-    if (isAdult === true) {
+    if (isAdult) {
       if (gender === 'M') {
         return '#3A8E5D';
       } else if (gender === 'F') {
         return '#0290B7';
       }
-    } else if (isStudent === true) {
+    } else if (isStudent) {
       return '#CB6342';
-    } else if (isChild === true) {
+    } else if (isChild) {
       return '#5AC9F5';
     } else {
       return '#56C4C4';
@@ -246,10 +215,10 @@ export const CoreMilestone: React.FC<IProps> = ({
     return '#56C4C4';
   };
 
-  const renderIconMileStone = (icnName: any, isActive?: boolean) => {
+  const renderIconMileStone = (iconName: any, isActive?: boolean) => {
     return (
       <StyledMilestoneIconWrapper
-        file={icnName}
+        file={iconName}
         size={Number(minorScale(3))}
         color={
           isActive
@@ -263,26 +232,24 @@ export const CoreMilestone: React.FC<IProps> = ({
   };
 
   return (
-    <StyledEmptyViewWrapper testID="coreMilestone">
-      <StyledViewIconWrapper>
-        <StyledAcceptChristWrapper testID="accept-christ">
-          {renderIconMileStone(ShapeHeart, hasAcceptedChrist)}
-        </StyledAcceptChristWrapper>
-        <StyledBaptisedWrapper testID="baptised">
-          {renderIconMileStone(Baptism, isBaptised)}
-        </StyledBaptisedWrapper>
-        {renderClassMileStoneIcon()}
-        <StyledSmallGroupWrapper>
-          {renderIconMileStone(People, isInSmallGroup)}
-        </StyledSmallGroupWrapper>
-        <StyledInMinistryWrapper>
-          {renderIconMileStone(ServingOpportunity, isInMinistry)}
-        </StyledInMinistryWrapper>
-        <StyledActiveMissionWrapper>
-          {renderIconMileStone(ShapeExperiences, isActiveInMissions)}
-        </StyledActiveMissionWrapper>
-      </StyledViewIconWrapper>
-    </StyledEmptyViewWrapper>
+    <StyledViewIconsWrapper testID="coreMilestone">
+      <StyledAcceptChristWrapper testID="accept-christ">
+        {renderIconMileStone(IconShapeHeart, hasAcceptedChrist)}
+      </StyledAcceptChristWrapper>
+      <StyledIconViewWrapper testID="baptised">
+        {renderIconMileStone(IconBaptism, isBaptised)}
+      </StyledIconViewWrapper>
+      {renderClassMileStoneIcon()}
+      <StyledIconViewWrapper>
+        {renderIconMileStone(IconPeople, isInSmallGroup)}
+      </StyledIconViewWrapper>
+      <StyledIconViewWrapper>
+        {renderIconMileStone(IconServingOpportunity, isInMinistry)}
+      </StyledIconViewWrapper>
+      <StyledIconViewWrapper>
+        {renderIconMileStone(IconShapeExperiences, isActiveInMissions)}
+      </StyledIconViewWrapper>
+    </StyledViewIconsWrapper>
   );
 };
 
--- src/modules/core-milestone/core-milestone.types.ts
+++ src/modules/core-milestone/core-milestone.types.ts
@@ -1,58 +1,58 @@
 export interface IProps {
-  /* if core milestone icon need white */
+  /* Pass true if white Icon for core milestone */
   isWhite?: boolean | false;
 
-  /* pass hasAttendedClass101 */
+  /* Pass True if hasAttendedClass101 active by default is false */
   hasAttendedClass101?: boolean | false;
 
-  /* pass hasAttendedClass201 */
+  /* Pass True if hasAttendedClass201 active by default is false */
   hasAttendedClass201?: boolean | false;
 
-  /* pass hasAttendedClass301 */
+  /* Pass True if hasAttendedClass301 active by default is false */
   hasAttendedClass301?: boolean | false;
 
-  /* pass hasAttendedClass401 */
+  /* Pass True if hasAttendedClass401 active by default is false */
   hasAttendedClass401?: boolean | false;
 
-  /* pass hasSignedMembershipAgreement */
+  /* Pass True if hasSignedMembershipAgreement active by default is false */
   hasSignedMembershipAgreement?: boolean | false;
 
-  /* pass hasSignedMaturityCovenant */
+  /* Pass True if hasSignedMaturityCovenant active by default is false */
   hasSignedMaturityCovenant?: boolean | false;
 
-  /* pass hasSignedMinistryCovenant */
+  /* Pass True if hasSignedMinistryCovenant active by default is false */
   hasSignedMinistryCovenant?: boolean | false;
 
-  /* pass hasSignedMissionCovenant */
+  /* Pass True if hasSignedMissionCovenant active by default is false */
   hasSignedMissionCovenant?: boolean | false;
 
-  /* pass hasAcceptedChrist */
+  /* Pass True if hasAcceptedChrist active by default is false */
   hasAcceptedChrist?: boolean | false;
 
-  /* pass isBaptised */
+  /* Pass True if isBaptised active by default is false */
   isBaptised?: boolean | false;
 
-  /* pass isInSmallGroup */
+  /* Pass True if isInSmallGroup active by default is false */
   isInSmallGroup?: boolean | false;
 
-  /* pass isInMinistry */
+  /* Pass True if isInMinistry active by default is false */
   isInMinistry?: boolean | false;
 
-  /* pass isActiveInMissions */
+  /* Pass True if isActiveInMissions active by default is false */
   isActiveInMissions?: boolean | false;
 
-  /* pass isAdult */
+  /* Pass True if isAdult active by default is false */
   isAdult?: boolean | false;
 
-  /* pass isStudent */
+  /* Pass True if isStudent active by default is false */
   isStudent?: boolean | false;
 
-  /* pass isChild */
+  /* Pass True if isChild active by default is false */
   isChild?: boolean | false;
 
-  /* pass isCgenderhild */
+  /* Pass True if isCgenderhild active by default is false */
   gender?: string | null | undefined;
 
-  /* pass color Milestone for class */
+  /* Pass color for class milestone for view styles */
   colorMilestone?: string | '#dde1e4';
 }
