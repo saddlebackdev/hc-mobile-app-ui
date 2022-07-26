214080ddc1d85cb74696bf79507c47021b4a53b5
yogeshpatel1404 <yogesh.patel@technossus.com>
2022-07-26 16:23:51 +0530

    AB#76301 - Review Person Record in ui repo and made changes in it

--- src/modules/person-record/person-record.types.ts
+++ src/modules/person-record/person-record.types.ts
@@ -17,24 +17,6 @@ export interface ITab {
 
 // Person Record
 export interface IProps {
-  /** Function to be called when the call icon is pressed. */
-  onPressCall: Function;
-
-  /** Function to be called when the sms icon is pressed. */
-  onPressSms: Function;
-
-  /** Function to be called when the email icon is pressed. */
-  onPressEmail: Function;
-
-  /** Show a confirmation prompt on click of "Call" icon. Defaults to true. */
-  shouldShowCallConfirmation?: boolean;
-
-  /** Show a confirmation prompt on click of "Email" icon. Defaults to true. */
-  shouldShowEmailConfirmation?: boolean;
-
-  /** Show a confirmation prompt on click of "SMS" icon. Defaults to true. */
-  shouldShowSmsConfirmation?: boolean;
-
   /** Basic details of this person. */
   person: {
     age: number;
@@ -52,24 +34,6 @@ export interface IProps {
     isStudent?: boolean;
     isChild?: boolean;
   };
-
-  /** Contact preferences of this person. */
-  contactPreferences: {
-    preferredMethod: string | null;
-  };
-
-  /** List of email addresses of this person. */
-  emailAddresses: Array<{
-    email: string | null;
-    isPrimary?: boolean;
-  }>;
-
-  /** List of phone numbers of this person. */
-  phoneNumbers: Array<{
-    phoneNumber: string | null;
-    isPrimary?: boolean;
-  }>;
-
   /** List of milestones of this person. */
   milestones: {
     firstContactDate: string | null;
