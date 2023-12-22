e20d8b2e33909fc9a97a960336a90182050da0d1
hareshTech <haresh.r@technossus.com>
2023-12-22 14:00:29 +0530

    Updated Snapshot

--- src/modules/appDown/appDown.tsx
+++ src/modules/appDown/appDown.tsx
@@ -13,14 +13,11 @@ import {SafeAreaView} from 'react-native';
 import Button from '../button/button';
 
 const StyledCard = Styled.View`
-  width: 100%;
-  height: 100%;
-`;
-
-const StyledCardSub = Styled.View`
-  justify-content: center;
+ justify-content: center;
   padding-horizontal: 45px;
   padding-vertical: 45px;
+  width: 100%;
+  height: 100%;
 `;
 
 const AnimationWrapper = Styled.View`
@@ -47,25 +44,23 @@ export const AppDown: React.FC<IProps> = ({
   <React.Fragment>
     <SafeAreaView>
       <StyledCard testID="app-Down-main">
-        <StyledCardSub>
-          <AnimationWrapper>{AnimationElement}</AnimationWrapper>
-          <HeadingWrapper>
-            <Heading variant="h2" alignment="center">
-              We are down for maintanence.
-            </Heading>
-          </HeadingWrapper>
-          <TextWrapper>
-            <Text variant="body1" alignment="center">
-              We&apos;re busy making some improvements to the app. Please check
-              back in a bit. Thanks for your patience!
-            </Text>
-          </TextWrapper>
-          <ButtonWrapper>
-            <Button testID="link" onPress={onRestart}>
-              Restart Application
-            </Button>
-          </ButtonWrapper>
-        </StyledCardSub>
+        <AnimationWrapper>{AnimationElement}</AnimationWrapper>
+        <HeadingWrapper>
+          <Heading variant="h2" alignment="center">
+            We are down for maintanence.
+          </Heading>
+        </HeadingWrapper>
+        <TextWrapper>
+          <Text variant="body1" alignment="center">
+            We&apos;re busy making some improvements to the app. Please check
+            back in a bit. Thanks for your patience!
+          </Text>
+        </TextWrapper>
+        <ButtonWrapper>
+          <Button testID="link" onPress={onRestart}>
+            Restart Application
+          </Button>
+        </ButtonWrapper>
       </StyledCard>
     </SafeAreaView>
   </React.Fragment>
